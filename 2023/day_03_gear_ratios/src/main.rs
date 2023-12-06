2e46d28bc4289a876abd527b7cd52dc52093c307
Daniel <daniel@not_of_your_interest.com>
2023-12-06 22:01:24 +0100

    WIP: day3 part 2

--- 2023/day_03_gear_ratios/src/main.rs
+++ 2023/day_03_gear_ratios/src/main.rs
@@ -1,9 +1,12 @@
-use std::fs;
+use std::{
+    collections::{HashMap, HashSet},
+    fs,
+};
 
 fn main() {
-    let content = fs::read_to_string("./input.txt").expect("Could not read file");
+    let content = fs::read_to_string("./test_input.txt").expect("Could not read file");
     part1(&content);
-    // part2(&content);
+    part2(&content);
 }
 
 fn check_symbol(map: &Vec<Vec<char>>, x: usize, y: usize) -> bool {
@@ -36,6 +39,31 @@ fn check_if_near_symbol(map: &Vec<Vec<char>>, x: usize, y: usize) -> bool {
     return false;
 }
 
+fn is_gear(map: &Vec<Vec<char>>, x: usize, y: usize) -> bool {
+    return map[x][y] == '*';
+}
+
+fn get_gears(map: &Vec<Vec<char>>, x: usize, y: usize) -> HashSet<(usize, usize)> {
+    let mut gears = HashSet::new();
+    let mut x_lower_bound: usize = 0;
+    if x > 1 {
+        x_lower_bound = x - 1;
+    }
+    let mut y_lower_bound: usize = 0;
+    if y > 1 {
+        y_lower_bound = y - 1;
+    }
+    for x_ref in x_lower_bound..x + 2 {
+        for y_ref in y_lower_bound..y + 2 {
+            // println!("{}, {}", x_ref, y_ref);
+            if x_ref < map.len() && y_ref < map[0].len() {
+                gears.insert((x, y));
+            }
+        }
+    }
+    return gears;
+}
+
 fn part1(content: &String) {
     let mut map: Vec<Vec<char>> = Vec::new();
     let mut sum = 0;
@@ -61,38 +89,58 @@ fn part1(content: &String) {
         }
     }
     println!("Part 1: {}", sum)
+}
 
-    // let mut sum = 0;
-    // for line in content.split("\n") {
-    //     let line_parts: Vec<&str> = line.split(":").collect::<Vec<&str>>();
-    //     let game = line_parts[0].split(" ").collect::<Vec<&str>>()[1]
-    //         .parse::<u32>()
-    //         .unwrap();
-    //     let cube_rounds = line_parts[1];
-    //     let mut skip: bool = false;
-    //     for cube_round in cube_rounds.split(";") {
-    //         for cube in cube_round.split(",") {
-    //             let cube_parts = cube.trim().split(" ").collect::<Vec<&str>>();
-    //             let amount = cube_parts[0].parse::<u32>().expect("Expected a number");
-    //             let colour = cube_parts[1];
+fn part2(content: &String) {
+    let mut map: Vec<Vec<char>> = Vec::new();
+    let mut sum = 0;
+    for line in content.split("\n") {
+        let chars = line.chars().collect();
+        map.push(chars);
+    }
+    let mut number_string = String::from("");
+    let mut near_symbol = false;
+    let mut gear_dict: HashMap<(usize, usize), Vec<u32>> = HashMap::new();
+    let mut found_gears: HashSet<(usize, usize)> = HashSet::new();
+    for x in 0..map.len() {
+        for y in 0..map[0].len() {
+            if map[x][y].is_digit(10) {
+                number_string.push(map[x][y]);
+                found_gears.extend(get_gears(&map, x, y).iter());
+            } else if number_string != "" {
+                assign_number_to_gear(&mut found_gears, &mut gear_dict, &mut number_string);
+                // count_number(&mut near_symbol, &mut number_string, &mut sum);
+            }
+        }
+        if number_string != "" && near_symbol {
+            assign_number_to_gear(&mut found_gears, &mut gear_dict, &mut number_string);
+            // count_number(&mut near_symbol, &mut number_string, &mut sum);
+        }
+    }
+    for (gear, numbers) in gear_dict.iter() {
+        if numbers.len() == 2 {
+            sum += numbers[0] * numbers[1];
+        }
+    }
+    println!("Part 2: {}", sum)
+}
 
-    //             if colour == "green" && amount > 13
-    //                 || colour == "red" && amount > 12
-    //                 || colour == "blue" && amount > 14
-    //             {
-    //                 skip = true;
-    //                 break;
-    //             }
-    //         }
-    //         if skip == true {
-    //             break;
-    //         }
-    //     }
-    //     if !skip {
-    //         sum += game;
-    //     }
-    // }
-    // println!("Part 1: {}", sum);
+fn assign_number_to_gear(
+    found_gears: &mut HashSet<(usize, usize)>,
+    gear_dict: &mut HashMap<(usize, usize), Vec<u32>>,
+    number_string: &mut String,
+) {
+    let number = number_string.parse::<u32>().expect("Expected a number");
+    *number_string = String::from("");
+    for gear in &*found_gears {
+        if !gear_dict.contains_key(&gear) {
+            gear_dict.insert(*gear, Vec::new());
+        }
+        gear_dict.get_mut(gear).unwrap().push(number);
+    }
+    println!("{}", number);
+    println!("{:?}", gear_dict);
+    found_gears.clear();
 }
 
 fn count_number(near_symbol: &mut bool, number_string: &mut String, sum: &mut u32) {
